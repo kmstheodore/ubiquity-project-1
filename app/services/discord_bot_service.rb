7104b70877db0a016cc6e982fe6d7c22bbad70b6
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2024-11-06 06:15:11 +0000

    add scheduled notif for event strike discord2

--- app/services/discord_bot_service.rb
+++ app/services/discord_bot_service.rb
@@ -12,6 +12,12 @@ class DiscordBotService
   def notifier(reminder)
     user = reminder.user
     discord_id = user.discord_id
-    @bot.user(discord_id).pm("Hello! A new reminder has been created: #{reminder.name}")
+    discord_user = @bot.user(discord_id)
+
+    if discord_user
+      discord_user.pm("Hello! A new reminder has been created: #{reminder.name}")
+    else
+      Rails.logger.error "Discord user with ID #{discord_id} not found"
+    end
   end
 end
