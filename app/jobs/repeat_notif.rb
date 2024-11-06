2ea2d18a80907b5515ea60a085e05660a6fdb6c0
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2024-11-06 05:41:39 +0000

    Ensure Puma binds to the correct port

---
+++ app/jobs/repeat_notif.rb
@@ -0,0 +1,8 @@
+class RepeatNotifJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+    Rails.logger.info "Test job executed with args: #{args.inspect}"
+  end
+end
