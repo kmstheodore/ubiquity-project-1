2b9ae7fac9a500be1b334517ac953a86fa1997a8
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2024-11-09 17:25:28 +0000

    redo index view style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-
   private
 
   def authenticate_user!
-    unless user_signed_in?
+    unless user_signed_in? || allowed_pages.include?(action_name)
       redirect_to request_demo_path
     end
   end
@@ -16,5 +14,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :discord_id ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :discord_id ])
+  end
+
+  def allowed_pages
+    %w[request_demo new create]
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :request_demo ]
   def request_demo
   end
 end
