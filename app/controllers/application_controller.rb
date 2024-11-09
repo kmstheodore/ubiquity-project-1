e29f29ce006875ccae5c223e44dfb85db44730c0
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2024-11-09 17:33:01 +0000

    fix auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :authenticate_user!
+  skip_before_action :authenticate_user!, if: :devise_controller?
   private
 
   def authenticate_user!
