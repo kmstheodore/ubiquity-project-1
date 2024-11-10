ceb12b91332044f44e60803dfa80d0657f9e6edc
kmstheodore <108134766+kmstheodore@users.noreply.github.com>
2024-11-10 13:28:32 +0000

    firebase test

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "firebase/app", to: "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js" # Adjust the version as needed
